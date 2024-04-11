742067a531b76fc04a6f742a1ad913b769d7d39e
lauravp84 <145235659+lauravp84@users.noreply.github.com>
2024-04-11 10:56:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+setInterval(showTime, 1000);
+
+function showTime(){
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    let am_pm = "AM";
+
+    if (hour >= 12) {
+        am_pm = "PM";
+        if (hour > 12) hour -= 12;
+    } else if (hour == 0) {
+        hour = 12;
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min; 
+    sec = sec < 10 ? "0" + sec : sec;
+    
+    let currentTime = 
+        hour + 
+        ":" + 
+        min + 
+        ":" + 
+        sec + 
+        " " + 
+        am_pm;
+
+    document.getElementById("clock").innerHTML = currentTime;
+}
+
+showTime();
